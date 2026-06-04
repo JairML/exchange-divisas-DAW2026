bc9a0006571cd9c40a9788816a54600dc7b4c800
csMattias09 <22200125@ue.edu.pe>
2026-06-04 12:23:48 -0500

    US-014 - Generacion de oferta de venta _real

---
+++ X-Chang.CORE/Core/Interfaces/IOfertaService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Dynamic.Core;
+using X_Chang.CORE.DTOs;
+
+namespace X_Chang.CORE.Interfaces;
+
+public interface IOfertaService
+{
+    Task<OfertaDto> CrearOfertaVentaAsync(int usuarioId, CrearOfertaRequest request);
+    Task<PagedResult<OfertaDto>> ObtenerMisOfertasAsync(int usuarioId, FiltroOfertasRequest filtro);
+    Task<OfertaDto> ObtenerOfertaAsync(int usuarioId, int ofertaId);
+    Task CancelarOfertaAsync(int usuarioId, int ofertaId);
+}
