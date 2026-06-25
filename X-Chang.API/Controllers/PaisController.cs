638679d642cc3605b138d0595192953c4c6d2f29
domenico-chang <24100033@ue.edu.pe>
2026-06-24 20:54:27 -0500

    Agrega parMonedaId y endpoint de paises

---
+++ X-Chang.API/Controllers/PaisController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X_Chang.CORE.Infrastructure.Data;
+
+namespace X_Chang.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaisController : ControllerBase
+    {
+        private readonly ExchangeDivisasDbContext _context;
+
+        public PaisController(ExchangeDivisasDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var paises = await _context.Paises
+                .Include(p => p.Moneda)
+                .OrderBy(p => p.Nombre)
+                .Select(p => new
+                {
+                    paisId = p.PaisId,
+                    nombre = p.Nombre,
+                    monedaId = p.MonedaId,
+                    codigoMoneda = p.Moneda.CodigoIso
+                })
+                .ToListAsync();
+
+            return Ok(paises);
+        }
+    }
+}
